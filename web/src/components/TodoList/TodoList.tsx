5dfd88209699d5b48a0455617174f0c32c776f0e
zjy4fun <zjy4fun@gmail.com>
2024-10-10 02:32:02 +0800

    fix build errors

--- web/src/components/TodoList/TodoList.tsx
+++ web/src/components/TodoList/TodoList.tsx
@@ -1,57 +1,65 @@
-import React, {useEffect, useRef, useState} from 'react';
-import './TodoList.css';
-import _ from 'lodash';
-import type {TodoItem} from '../../types/TodoItem.ts';
-import {formatTime} from "../../utils/time.ts";
+import { useRef, useState } from "react";
+import "./TodoList.css";
+import type { TodoItem } from "../../types/TodoItem.ts";
+import { formatTime } from "../../utils/time.ts";
 
 interface TodoListProps {
-    todos: TodoItem[];
-    setTodos: (todos: TodoItem[]) => void;
-    currentTodo: TodoItem | null;
-    setCurrentTodo: (todoItem: TodoItem) => void;
-    handleStartPress: (event: Event, todo: TodoItem) => void;
-    deleteTodo: (todoItem: TodoItem) => void;
+  todos: TodoItem[];
+  setTodos: (todos: TodoItem[]) => void;
+  currentTodo: TodoItem | null;
+  setCurrentTodo: (todoItem: TodoItem) => void;
+  handleStartPress: (event: React.MouseEvent<HTMLButtonElement>, todo: TodoItem) => void;
+  deleteTodo: (todoItem: TodoItem) => void;
 }
 
 export const TodoList = ({
-                             todos,
-                             setTodos,
-                             currentTodo,
-                             setCurrentTodo,
-                             handleStartPress,
-                             deleteTodo
-                         }: TodoListProps) => {
-    const [selectedTodo, setSelectedTodo] = useState<TodoItem | null>(null);
-    const newTodoRef = useRef<HTMLInputElement>(null);
+  todos,
+  currentTodo,
+  handleStartPress,
+  deleteTodo,
+}: TodoListProps) => {
+  const [selectedTodo, setSelectedTodo] = useState<TodoItem | null>(null);
+  const newTodoRef = useRef<HTMLInputElement>(null);
 
-    return (
-        <div className="todo-list-container">
-            <div className="todo-items">
-                {todos.map((todo, index) => (
-                    <div
-                        key={index}
-                        ref={index === todos.length - 1 ? newTodoRef : null}
-                        className={`todo-item ${selectedTodo?.id === todo.id ? 'selected' : ''}`}
-                        onClick={() => setSelectedTodo(todo)}
-                    >
-                        {formatTime(todo.timeStamp)}
-                        {'    -    '}
-                        {todo.text}
-                        <div className={'btn-container'}>
-                            <button onClick={(event) => {
-                                handleStartPress(event, todo)
-                            }}
-                                    className={`start-button ${currentTodo?.id === todo.id ? 'stop' : ''}`}
-                            >{currentTodo?.id === todo.id ? 'Stop' : 'Start'}</button>
-                            <button onClick={(event) => {
-                                event.stopPropagation();
-                                deleteTodo(todo);
-                            }} className="delete-button">Delete
-                            </button>
-                        </div>
-                    </div>
-                ))}
+  return (
+    <div className="todo-list-container">
+      <div className="todo-items">
+        {todos.map((todo, index) => (
+          <div
+            key={index}
+            ref={index === todos.length - 1 ? newTodoRef : null}
+            className={`todo-item ${
+              selectedTodo?.id === todo.id ? "selected" : ""
+            }`}
+            onClick={() => setSelectedTodo(todo)}
+          >
+            {formatTime(todo.timeStamp)}
+            {"    -    "}
+            {todo.text}
+            <div className={"btn-container"}>
+              <button
+                onClick={(event) => {
+                  handleStartPress(event as React.MouseEvent<HTMLButtonElement>, todo);
+                }}
+                className={`start-button ${
+                  currentTodo?.id === todo.id ? "stop" : ""
+                }`}
+              >
+                {currentTodo?.id === todo.id ? "Stop" : "Start"}
+              </button>
+              <button
+                onClick={(event) => {
+                  event.stopPropagation();
+                  deleteTodo(todo);
+                }}
+                className="delete-button"
+              >
+                Delete
+              </button>
             </div>
-        </div>
-    );
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
--- web/src/pages/App/App.tsx
+++ web/src/pages/App/App.tsx
@@ -1,91 +1,95 @@
-import {TodoList} from "../../components/TodoList/TodoList.tsx";
-import {Current} from "../../components/Current/Current.tsx";
-import {useEffect, useRef, useState} from 'react';
-import './App.css';
-import {TodoItem} from "../../types/TodoItem.ts";
-import {TodoInput} from "../../components/TodoInput/TodoInput.tsx";
+import { TodoList } from "../../components/TodoList/TodoList.tsx";
+import { Current } from "../../components/Current/Current.tsx";
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
+import { TodoItem } from "../../types/TodoItem.ts";
+import { TodoInput } from "../../components/TodoInput/TodoInput.tsx";
 
 export const App = () => {
-    const [currentTodo, setCurrentTodo] = useState<TodoItem | null>(null);
-    const [todos, setTodos] = useState<TodoItem[]>([]);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const timer = useRef<number | undefined>(undefined);
+  const [currentTodo, setCurrentTodo] = useState<TodoItem | null>(null);
+  const [todos, setTodos] = useState<TodoItem[]>([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const timer = useRef<number | undefined>(undefined);
 
+  const loadTodosFromLocalStorage = () => {
+    const savedTodos = localStorage.getItem("todos");
+    if (savedTodos) {
+      setTodos(JSON.parse(savedTodos));
+    }
+  };
+
+  useEffect(() => {
+    loadTodosFromLocalStorage();
+    setIsLoaded(true);
+  }, []);
+
+  useEffect(() => {
     const saveTodosToLocalStorage = () => {
-        localStorage.setItem('todos', JSON.stringify(todos));
+      localStorage.setItem("todos", JSON.stringify(todos));
     };
-
-    const loadTodosFromLocalStorage = () => {
-        const savedTodos = localStorage.getItem('todos');
-        if (savedTodos) {
-            setTodos(JSON.parse(savedTodos));
-        }
-    };
-
-    useEffect(() => {
-        loadTodosFromLocalStorage();
-        setIsLoaded(true);
-    }, []);
-
-    useEffect(() => {
-        if (isLoaded) {
-            saveTodosToLocalStorage();
-        }
-    }, [todos, isLoaded]);
-
-    const addTime = (todo: TodoItem) => {
-        timer.current = undefined;
-        timer.current = setTimeout(() => {
-            if (!todos.find((t) => t.id === todo.id)) {
-                clearTimeout(timer?.current);
-                timer.current = undefined;
-                return;
-            }
-            setTodos((preTodos) => {
-                return preTodos.map((t) => {
-                    if (t.id === todo.id) {
-                        return {
-                            ...t,
-                            timeStamp: t.timeStamp + 1000
-                        };
-                    }
-                    return t;
-                });
-            })
-            addTime(todo);
-        }, 1000)
+    if (isLoaded) {
+      saveTodosToLocalStorage();
     }
+  }, [todos, isLoaded]);
 
-    const handleStartPress = (event: Event, todo: TodoItem) => {
-        event.stopPropagation();
-        clearTimeout(timer.current);
+  const addTime = (todo: TodoItem) => {
+    timer.current = undefined;
+    timer.current = setTimeout(() => {
+      if (!todos.find((t) => t.id === todo.id)) {
+        clearTimeout(timer?.current);
         timer.current = undefined;
-        if (currentTodo?.id === todo.id) {
+        return;
+      }
+      setTodos((preTodos) => {
+        return preTodos.map((t) => {
+          if (t.id === todo.id) {
+            return {
+              ...t,
+              timeStamp: t.timeStamp + 1000,
+            };
+          }
+          return t;
+        });
+      });
+      addTime(todo);
+    }, 1000);
+  };
+
+  const handleStartPress = (event: React.MouseEvent<HTMLButtonElement>, todo: TodoItem) => {
+    event?.stopPropagation();
+    clearTimeout(timer.current);
+    timer.current = undefined;
+    if (currentTodo?.id === todo.id) {
+      setCurrentTodo(null);
+      return;
+    }
+    setCurrentTodo(todo);
+    addTime(todo);
+  };
+
+  return (
+    <div className={"container"}>
+      <TodoList
+        todos={todos}
+        setTodos={setTodos}
+        currentTodo={currentTodo}
+        setCurrentTodo={setCurrentTodo}
+        handleStartPress={handleStartPress}
+        deleteTodo={(todo) => {
+          if (currentTodo?.id === todo.id) {
             setCurrentTodo(null);
-            return;
-        }
-        setCurrentTodo(todo);
-        addTime(todo);
-    }
-
-    return (
-        <div className={'container'}>
-            <TodoList
-                todos={todos}
-                setTodos={setTodos}
-                currentTodo={currentTodo}
-                setCurrentTodo={setCurrentTodo}
-                handleStartPress={handleStartPress}
-                deleteTodo={(todo) => {
-                    if (currentTodo?.id === todo.id) {
-                        setCurrentTodo(null);
-                    }
-                    setTodos(todos.filter((t) => t.id !== todo.id));
-                }}/>
-            <Current currentTodo={todos.filter(todo => todo.id === currentTodo?.id)?.[0]}/>
-            <TodoInput addTodo={(todoItem) => {
-                setTodos([...todos, todoItem]);
-            }}/>
-        </div>
-    )
-}
+          }
+          setTodos(todos.filter((t) => t.id !== todo.id));
+        }}
+      />
+      <Current
+        currentTodo={todos.filter((todo) => todo.id === currentTodo?.id)?.[0]}
+      />
+      <TodoInput
+        addTodo={(todoItem) => {
+          setTodos([...todos, todoItem]);
+        }}
+      />
+    </div>
+  );
+};
